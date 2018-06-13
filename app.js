b348d920e53608eaeed7e80abaf5563eee745e7f
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-13 11:10:10 -0500

    restructering a lot of the code
    makign this more clean, following the video example more becuase it makes more sense than what ever mess I had going on

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
 //https://css-tricks.com/examples/ShapesOfCSS/
+const canvasHeight = 600;
+const canvasWidth = 600;
+let canvas = document.querySelector('.canvas');
+
+
 
 class Shape {
-    constructor(height, width, x, y) {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+        this.draw();
+    }
+    
+    draw() {
         this.div = document.createElement('div');
-        this.div.style.height = `${height}px`;
-        this.div.style.width = `${width}px`;
+        let x = Math.floor(Math.random() * (canvasWidth - this.width));
+        let y = Math.floor(Math.random() * (canvasHeight - this.height));
         this.div.style.left = `${x}px`;
         this.div.style.top = `${y}px`;
+        this.div.style.height = `${height}px`;
+        this.div.style.width = `${width}px`;
+        
+        this.div.addEventListener('click', () => {
+            console.log('clicked');    
+        });
+        this.div.addEventListener('dblclick', () => {
+            console.log('dblclicked');    
+        });
+
+        canvas.appendChild(this.div);
     }
-}
+};
 
 class Rectangle extends Shape {
     constructor(height, width, x, y) {
         super(height, width, x, y);
-        this.div.classList.add('rectangle');
+        this.div.classList.add('rectangle', 'shape');
+   }
 
-    }
-}
+   area () {
+       return this.width * this.height;
+   }
 
+   perimeter() { 
+       return 2 * this.width + 2 * this.height;
+   }
+};
 class Square extends Rectangle {
-    constructor(height, width, x, y) {
-        super(height, width, x, y);
-        this.div.classList.add('square');
+    constructor(height, width) {
+        super(height, width);
+        this.div.classList.add('square', 'shape');
     }
-}
-
+};
 class Circle extends Shape {
     constructor(radius) {
         super(2 * radius, 2 * radius)
         this.radius = radius;
+        this.div.classList.add('circle', 'shape')
     }
 
     area () {
@@ -38,11 +66,12 @@ class Circle extends Shape {
     circum() {
         return 2 * Math.PI * this.radius;
     }
-}
+};
 
 class Triangle extends Shape {
     constructor(height){
         super(height, height);
+        this.div.classList.add('triangle', 'shape')
     }
     area() {
         return 0.5 * this.height * this.height;
@@ -51,33 +80,31 @@ class Triangle extends Shape {
         return 2 * this.height + Math.sqrt(2) * this.height;
     }
 
-}
-
-
-// to create the random placement. 
-function randVal() {
-    return Math.floor(Math.random() * (600 - 1)) + 1
 };
 
-//playing with event listner here
-document.getElementById('rectangleform').addEventListener("submit", function (e) {
-    e.preventDefault();
-    let rectHeight = document.getElementById('rectheight').value;
-    let rectWidth = document.getElementById('rectwidth').value;
-    let newRect = new Rectangle(rectHeight, rectWidth, randVal(), randVal());
-    let canvas = document.querySelector('.canvas');
-    canvas.appendChild(newRect.div);
-});
+//playing with event listener here
+document.getElementById('rectangleform').addEventListener("submit", createRect());
+
 document.getElementById('sqrform').addEventListener("submit", function (e) {
     e.preventDefault();
-    let sqrHeight = document.getElementById('sqrsize').value;
-    let sqrWidth = document.getElementById('sqrsize').value;
-    let newSqr = new Square(sqrHeight, sqrWidth, randVal(), randVal());
-    let canvas = document.querySelector('.canvas');
-    canvas.appendChild(newSqr.div);
+    let newSqr = new Square(sqrHeight, sqrWidth);
+    
 });
 
 document.getElementById('circont').addEventListener("submit", function (e) {
     e.preventDefault();
-
+    let newCir = new Circle(cirCircum, x, y);
+    
+    
+    
 });
+let rectHeight = document.getElementById('rectheight').value;
+let rectWidth = document.getElementById('rectwidth').value;
+let cirCircum = document.getElementById('cirsize').value;
+let sqrHeight = document.getElementById('sqrsize').value;
+let sqrWidth = document.getElementById('sqrsize').value;
+
+function createRect(){
+    new Rectangle(rectHeight, rectWidth);
+  
+}
